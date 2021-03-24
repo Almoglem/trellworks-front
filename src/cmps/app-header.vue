f6f14294bd0519dda7aff0616736c6482aa4ba85
Almog <almoglemberg@gmail.com>
2021-03-24 17:42:11 +0200

    scss changes

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -1,32 +1,32 @@
 <template>
-	<header class="flex">
-		<nav class="flex">
-			<router-link class="header-btn transition" to="/">Home</router-link>
-			<router-link class="header-btn transition" to="/boards"
-				>Boards</router-link
-			>
-		</nav>
-		<router-link to="/">
-			<span class="main-logo" aria-label="logo">Trellworks</span>
-		</router-link>
-		<section class="flex">
-			<p v-if="loggedInUser"><avatar :size="30" :username="loggedInUser.fullname"></avatar></p>
-			<section v-else class="nav-side-buttons header-btn transition">
-				<router-link to="/login" >Login / Signup</router-link>
-			</section>
-		</section>
-	</header>
+  <header class="flex">
+    <nav class="flex">
+      <router-link class="header-btn transition" to="/boards"
+        >Boards</router-link
+      >
+    </nav>
+    <router-link to="/">
+      <span class="main-logo" aria-label="logo">Trellworks</span>
+    </router-link>
+    <section class="flex">
+      <p v-if="loggedInUser">
+        <avatar :size="30" :username="loggedInUser.fullname"></avatar>
+      </p>
+      <section v-else class="nav-side-buttons header-btn transition">
+        <router-link to="/login">Log in | Sign Up</router-link>
+      </section>
+    </section>
+  </header>
 </template>
 <script>
 import Avatar from "vue-avatar";
 
 export default {
-	computed: {
-		loggedInUser() {
-			return this.$store.getters.loggedinUser
-		}
-	},
-    components: { Avatar },
-
+  computed: {
+    loggedInUser() {
+      return this.$store.getters.loggedinUser;
+    },
+  },
+  components: { Avatar },
 };
 </script>
--- src/cmps/board/board-header.vue
+++ src/cmps/board/board-header.vue
@@ -1,130 +1,145 @@
 <template>
-	<div class="flex board-header">
-		<div class="flex header-container" @submit.prevent="editBoardTitle">
-			<input class="clean-input" v-model="titleToEdit" v-if="isEditing" ref="titleInput" 
-			@keypress.enter="editBoardTitle"
-			@focusout="editBoardTitle" />
-			<h2 @click="getInput" v-else>{{ boardTitle }}</h2>
-			<button class="header-btn star"><i class="far fa-star"></i></button>
-			<ul class="flex avatars-show">
-				<span
-					v-for="member in boardMembers"
-					:key="member._id"
-					class="clickable transition board-header-avatar"
-					@click="showProfile(member)"
-				>
-				<span @click="currMember = member">
-					<avatar :size="30" :username="member.fullname"></avatar
-				></span>
-				</span>
-				<memberProfile
-					:currMember="currMember"
-					v-if="showMemberProfile"
-					@closeProfile="hideProfile"
-				/>
-			</ul>
-			<button class="header-btn invite" @click="showUsers">Invite to Board</button>
-		</div>
-		<button @click="menuShown = !menuShown" class="header-btn">
-			Show Menu
-		</button>
-		<transition name="slide-from-right">
-			<boardMenu
-				class="board-menu"
-				v-if="menuShown"
-				@closeMenu="menuShown = !menuShown"
-				@bgcChanged="changeBgc"
-			></boardMenu>
-		</transition>
-		<div :class="{'pop-up-window': userWindow}" @click.self="userWindow = !userWindow">
-			<div v-if="userWindow" class="pop-up">
-				<h3 class="pop-up-title">Invite to board</h3>
-				<hr>
-			<li v-for="user in users" @click="toggleBoardMember(user)" :key="user._id">{{user.fullname}}</li>
-			</div>
-		</div>
-	</div>
+  <div class="flex board-header">
+    <div class="flex header-container" @submit.prevent="editBoardTitle">
+      <input
+        class="clean-input"
+        v-model="titleToEdit"
+        v-if="isEditing"
+        ref="titleInput"
+        @keypress.enter="editBoardTitle"
+        @focusout="editBoardTitle"
+      />
+      <h2 @click="getInput" v-else>{{ boardTitle }}</h2>
+      <button class="header-btn star"><i class="far fa-star"></i></button>
+      <ul class="flex avatars-show">
+        <span
+          v-for="member in boardMembers"
+          :key="member._id"
+          class="clickable transition board-header-avatar"
+          @click="showProfile(member)"
+        >
+          <span @click="currMember = member">
+            <avatar :size="30" :username="member.fullname"></avatar
+          ></span>
+        </span>
+        <memberProfile
+          :currMember="currMember"
+          v-if="showMemberProfile"
+          @closeProfile="hideProfile"
+        />
+      </ul>
+      <button class="header-btn invite" @click="showUsers">Invite</button>
+    </div>
+    <button @click="menuShown = !menuShown" class="header-btn">
+      Show Menu
+    </button>
+    <transition name="slide-from-right">
+      <boardMenu
+        class="board-menu"
+        v-if="menuShown"
+        @closeMenu="menuShown = !menuShown"
+        @bgcChanged="changeBgc"
+      ></boardMenu>
+    </transition>
+    <div
+      :class="{ 'pop-up-window': userWindow }"
+      @click.self="userWindow = !userWindow"
+    >
+      <div v-if="userWindow" class="pop-up">
+        <h3 class="pop-up-title">Invite to board</h3>
+        <hr />
+        <li
+          v-for="user in users"
+          @click="toggleBoardMember(user)"
+          :key="user._id"
+        >
+          {{ user.fullname }}
+        </li>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import boardMenu from "./menu.vue";
-import memberProfile from "../recurring-cmps/user-miniprofile.vue"
+import memberProfile from "../recurring-cmps/user-miniprofile.vue";
 import Avatar from "vue-avatar";
 
 export default {
-	props: {
-		boardTitle: {
-			type: String,
-			required: true,
-		},
-	},
-	data() {
-		return {
-			menuShown: false,
-			titleToEdit: this.boardTitle,
-			isEditing: false,
-			showMemberProfile: false,
-			currMember: null,
-			userWindow: false
-		};
-	},
-	computed: {
-		boardMembers() {
-			return this.$store.getters.currBoardMembers;
-		},
-		users(){
-			return this.$store.getters.users
-		},
-		currBoard(){
-			return JSON.parse(JSON.stringify(this.$store.getters.currBoard))
-		},
+  props: {
+    boardTitle: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      menuShown: false,
+      titleToEdit: this.boardTitle,
+      isEditing: false,
+      showMemberProfile: false,
+      currMember: null,
+      userWindow: false,
+    };
+  },
+  computed: {
+    boardMembers() {
+      return this.$store.getters.currBoardMembers;
+    },
+    users() {
+      return this.$store.getters.users;
+    },
+    currBoard() {
+      return JSON.parse(JSON.stringify(this.$store.getters.currBoard));
+    },
+  },
+  methods: {
+    async loadUsers() {
+      await this.$store.dispatch({ type: "loadUsers" });
+    },
+    editBoardTitle() {
+      this.isEditing = !this.isEditing;
+      this.$emit("boardTitleUpdated", this.titleToEdit);
+    },
+    showProfile(member) {
+      this.currMember = member;
+      this.showMemberProfile = true;
+    },
+    hideProfile() {
+      this.showMemberProfile = false;
+    },
+    changeBgc() {
+      this.$emit("changeBgc");
+    },
+    getInput() {
+      this.isEditing = !this.isEditing;
+      setTimeout(() => {
+        this.$refs.titleInput.focus();
+      }, 0);
+    },
+    showUsers() {
+      this.userWindow = !this.userWindow;
+    },
+    toggleBoardMember(user) {
+      const userToAdd = {
+        _id: user._id,
+        fullname: user.fullname,
+      };
 
-	},
-	methods: {
-		async loadUsers(){
-			await this.$store.dispatch({type: 'loadUsers'});
-		},
-		editBoardTitle() {
-			this.isEditing = !this.isEditing;
-			this.$emit("boardTitleUpdated", this.titleToEdit);
-		},
-		showProfile(member) {
-			this.currMember = member
-			this.showMemberProfile = true
-		},
-		hideProfile() {
-			this.showMemberProfile = false
-		},
-		changeBgc() {
-			this.$emit('changeBgc')
-		},
-		getInput(){
-			this.isEditing = !this.isEditing
-			setTimeout(() => {
-				this.$refs.titleInput.focus()
-			}, 0);
-		},
-		showUsers(){
-			this.userWindow = !this.userWindow
-		},
-		toggleBoardMember(user){
-			const userToAdd = {
-				_id: user._id,
-				fullname: user.fullname
-			}
-
-			const foundIdx = this.currBoard.members.findIndex(member => member._id === user._id)
-			if(foundIdx !== -1) {
-				this.currBoard.members.splice(foundIdx, 1)
-				return this.$emit('updateBoard', this.currBoard)
-			}
-			this.currBoard.members.push(userToAdd)
-			this.$emit('updateBoard', this.currBoard)
-		}	
-	},
-	async created() { 
-		await this.loadUsers()
-	},
-	components: { boardMenu, Avatar, memberProfile },
+      const foundIdx = this.currBoard.members.findIndex(
+        (member) => member._id === user._id
+      );
+      if (foundIdx !== -1) {
+        this.currBoard.members.splice(foundIdx, 1);
+        return this.$emit("updateBoard", this.currBoard);
+      }
+      this.currBoard.members.push(userToAdd);
+      this.$emit("updateBoard", this.currBoard);
+    },
+  },
+  async created() {
+    await this.loadUsers();
+  },
+  components: { boardMenu, Avatar, memberProfile },
 };
 </script>
