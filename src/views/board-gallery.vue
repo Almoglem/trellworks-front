8880a33c08c178aaeac60b79997146046edf5ca2
Gali Nickel <gali.nickel95@gmail.com>
2021-03-24 18:28:50 +0200

    board gallery trash

--- src/views/board-gallery.vue
+++ src/views/board-gallery.vue
@@ -1,20 +1,36 @@
 <template>
 	<section>
-		<app-header/>
+		<app-header />
 		<div class="flex main-container gallery">
+			<h1>Starred Boards</h1>
+			<div class="board-list">
+				<router-link
+					class="board-preview"
+					v-for="starBoard in starBoards"
+					:key="starBoard._id"
+					:to="'/board/' + starBoard._id"
+				>
+					<div class="placeholder-preview">
+							<i style="float:right" @click="deleteBoard(board._id)" class="far fa-trash-alt transition"></i>
+						<div class="transition preview-header">
+							<h2>{{ starBoard.title }}</h2>
+						</div>
+					</div>
+				</router-link>
+			</div>
 			<h1>Your boards</h1>
 			<div class="board-list">
 				<div @click="createBoard" class="placeholder-preview add">
 					<div class="vertical"><div class="horizontal"></div></div>
-					
 				</div>
 				<router-link
 					class="board-preview"
 					v-for="board in boards"
 					:key="board._id"
-					:to="'/board/' + board._id"	
+					:to="'/board/' + board._id"
 				>
 					<div class="placeholder-preview">
+							<i style="float:right" @click="deleteBoard(board._id)" class="far fa-trash-alt transition"></i>
 						<div class="transition preview-header">
 							<h2>{{ board.title }}</h2>
 						</div>
@@ -22,7 +38,7 @@
 				</router-link>
 			</div>
 		</div>
-		<loader v-if="isLoading"/>
+		<loader v-if="isLoading" />
 	</section>
 </template>
 
@@ -34,16 +50,16 @@ import loader from '@/cmps/recurring-cmps/loader'
 export default {
 	data() {
 		return {
-			isLoading: false
+			isLoading: false,
+			starBoards: []
 		}
 	},
 	computed: {
 		boards() {
-			//FIXME: no need to return whole board array with tasks inside. mini-boards array with titles and ids is enough
 			return this.$store.getters.boards
 		},
-		loggedInUser(){
-			return this.$store.getters.loggedinUser 
+		loggedInUser() {
+			return this.$store.getters.loggedinUser
 		}
 	},
 	methods: {
@@ -51,7 +67,7 @@ export default {
 			try {
 				this.isLoading = true;
 				await this.$store.dispatch({
-				type: 'loadBoards'
+					type: 'loadBoards'
 				})
 				this.isLoading = false;
 			} catch (err) {
@@ -64,22 +80,26 @@ export default {
 						title: 'error',
 						popup: 'error'
 					},
-					toast:true,
-					animation:true
-			})
+					toast: true,
+					animation: true
+				})
 			}
 		},
 		async createBoard() {
-			const loggedUser = this.loggedInUser||  {fullname: 'Guest'}
+			const loggedUser = this.loggedInUser || { fullname: 'Guest' }
 			await this.$store.dispatch({
 				type: 'newBoard',
 				loggedUser
 			})
 			this.loadBoards()
 		},
+		starredBoards() {
+			return this.boards.filter(board => board.isStarred)
+		}
 	},
 	async created() {
 		await this.loadBoards()
+		this.starBoards = this.starredBoards()
 	},
 	components: {
 		appHeader,
