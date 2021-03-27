ee27d346e1ea6ba4d73386dbdfc306fe86129e22
Almog <almoglemberg@gmail.com>
2021-03-27 14:40:50 +0300

    dashboard

--- src/cmps/board/dashboard.vue
+++ src/cmps/board/dashboard.vue
@@ -1,11 +1,30 @@
 <template>
-  <h1>HI IM DASHBOARD</h1>
+  <section class="dashboard-container">
+    <h1>This board's stats</h1>
+    <p>Created at {{ currBoard.createdAt }} (use moment JS here)</p>
+    <p>{{ currBoard.members.length }} members</p>
+    <p>{{ tasksLength }} tasks</p>
+    <p>{{ currBoard.activities.length }} activities</p>
+    <!-- {{ currBoard }} -->
+  </section>
 </template>
 
 <script>
 export default {
   props: {},
-  computed: {},
+  computed: {
+    currBoard() {
+      return this.$store.getters.currBoard;
+    },
+    tasksLength() {
+      const board = this.$store.getters.currBoard;
+      let count = 0;
+      for (let i = 0; i < board.groups.length; i++) {
+        count += board.groups[i].task.length;
+      }
+      return count;
+    },
+  },
   methods: {},
   components: {},
 };
