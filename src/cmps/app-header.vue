b4bbf09e71055405c1c6fa52f9848d5192fb29ab
Almog <almoglemberg@gmail.com>
2021-03-28 13:57:41 +0300

    logout fixed

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -6,51 +6,53 @@
     <router-link to="/">
       <span class="main-logo" aria-label="logo">Trellworks</span>
     </router-link>
-    
+
     <div class="flex" v-if="loggedInUser">
-							<userPic :size="30" :user="loggedInUser" @userClicked="togglePopUp(true)"/>
-
-
+      <userPic
+        :size="30"
+        :user="loggedInUser"
+        @userClicked="togglePopUp(true)"
+      />
+      <section v-if="popUpToggle" class="pop-up">
+        <h3 class="pop-up-title">User profile</h3>
+        <hr />
+        <ul class="about-user">
+          <li class="profile">
+            <div class="name-profile">
+              <span>{{ loggedInUser.fullname }}</span>
+              <small>User since: </small>
+            </div>
+            <div class="avatar-profile">
+              <img
+                v-if="loggedInUser.profileImg"
+                class="user-profileimg"
+                :src="loggedInUser.profileImg"
+                alt=""
+              />
+              <avatar
+                v-else
+                :size="30"
+                :username="loggedInUser.fullname"
+              ></avatar>
+              <label for="file-upload"><small>Edit profile image</small></label>
+              <input
+                type="file"
+                id="file-upload"
+                :style="{ display: 'none' }"
+                @change="updateProfileImage"
+              />
+            </div>
+          </li>
+          <hr />
+          <li class="link">
+            <a @click="doLogout">Log out</a>
+          </li>
+        </ul>
+      </section>
       <!-- <button class="header-btn" @click="doLogout">Log Out</button> -->
     </div>
     <section v-else class="nav-side-buttons header-btn transition">
       <router-link to="/login">Log in </router-link>
-    </section>
-    <section v-if="popUpToggle" class="pop-up">
-      <h3 class="pop-up-title">User profile</h3>
-      <hr />
-      <ul class="about-user">
-        <li class="profile">
-          <div class="name-profile">
-            <span>{{ loggedInUser.fullname }}</span>
-            <small>User since: </small>
-          </div>
-          <div class="avatar-profile">
-            <img
-              v-if="loggedInUser.profileImg"
-              class="user-profileimg"
-              :src="loggedInUser.profileImg"
-              alt=""
-            />
-            <avatar
-              v-else
-              :size="30"
-              :username="loggedInUser.fullname"
-            ></avatar>
-            <label for="file-upload"><small>Edit profile image</small></label>
-            <input
-              type="file"
-              id="file-upload"
-              :style="{ display: 'none' }"
-              @change="updateProfileImage"
-            />
-          </div>
-        </li> 
-        <hr>
-        <li class="link">
-          <a @click="doLogout">Log out</a>
-        </li>
-      </ul>
     </section>
     <div
       v-if="popUpToggle"
@@ -64,12 +66,12 @@
 import { uploadImg } from "@/services/img-upload.service";
 import loader from "@/cmps/recurring-cmps/loader";
 import userPic from "./recurring-cmps/user-pic.vue";
-
+import Avatar from "vue-avatar";
 export default {
   data() {
     return {
       popUpToggle: false,
-      isLoading: false
+      isLoading: false,
     };
   },
   computed: {
@@ -85,7 +87,7 @@ export default {
       this.popUpToggle = toggler;
     },
     async updateProfileImage(ev) {
-      this.isLoading = true
+      this.isLoading = true;
       try {
         const imgUploaded = await uploadImg(ev);
         this.loggedInUser.profileImg = imgUploaded.url;
@@ -94,25 +96,24 @@ export default {
           user: this.loggedInUser,
         });
       } catch (err) {
-          Swal.fire({
-            position: "bottom-end",
-            title: "Sorry, There was a problem with uploading your image.",
-            showConfirmButton: false,
-            timer: 1500,
-            customClass: {
-              title: "error",
-              popup: "error",
-            },
-            toast: true,
-            animation: true,
-          });
+        Swal.fire({
+          position: "bottom-end",
+          title: "Sorry, There was a problem with uploading your image.",
+          showConfirmButton: false,
+          timer: 1500,
+          customClass: {
+            title: "error",
+            popup: "error",
+          },
+          toast: true,
+          animation: true,
+        });
       } finally {
-        this.isLoading = false
+        this.isLoading = false;
       }
     },
   },
-  components: { loader,userPic },
-  created() {
-  },
+  components: { loader, userPic, Avatar },
+  created() {},
 };
 </script>
